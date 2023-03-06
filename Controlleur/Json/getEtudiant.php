9283fe86a929def8c2e38193c234bca9a4dcdea5
thibault <tybo2803@hotmail.fr>
2023-03-06 08:37:46 +0100

    bug utf8

--- Controlleur/Json/getEtudiant.php
+++ Controlleur/Json/getEtudiant.php
@@ -5,7 +5,10 @@ include_once pathForOs($_SERVER['DOCUMENT_ROOT'] . "/Modele/recupEtudiant.php");
 $etudiants = getEtudiants();
 
 if ($etudiants){
+    error_log("avant traitement");
+    error_log( $etudiants);
     $jsonEtudiants = json_encode($etudiants, JSON_UNESCAPED_UNICODE);
+    error_log("après traitement");
     error_log($jsonEtudiants);
     echo $jsonEtudiants;
 }else{
