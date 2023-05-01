b34b2b2e75d94dc4078eb8ac2e42fecfa7a97153
Lionreaux <tybo2803@hotmail.fr>
2023-05-01 16:23:35 +0200

    install

--- Modele/recupEtudiant.php
+++ Modele/recupEtudiant.php
@@ -5,7 +5,6 @@ function getEtudiants(){
     error_log("boubou");
     $conn = generateMysqliConnexion();
     $idClasse = $_GET['idClasse'];
-    error_log("boubou");
     error_log($idClasse);
     $stmt = $conn->prepare("SELECT E.nom FROM Etudiants E WHERE E.classe=?;");
     $stmt->bind_param("i", $idClasse);
